0ab968d403a77ab8cf2163e532578d9e12766365
Opilane TTHK <opilane@tthk.ee>
2021-09-15 12:14:22 +0300

    DateAndTimeObjects

---
+++ DateAndTimeObjects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateAndTimeObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime current = DateTime.Now;
+            Console.WriteLine(current.Hour);
+            Console.WriteLine(current.Minute);
+            Console.WriteLine(current.Second);
+            Console.WriteLine(current.Millisecond);
+            Console.WriteLine(current.ToString("dd MMMM"));
+            Console.WriteLine(current.ToString("MMMM"));
+            Console.WriteLine(current.ToString());
+        }
+    }
+}
